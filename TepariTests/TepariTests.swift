0e1d9541794195dc623d54966f336546d495e28b
Ash <ash@greenwoodpark.com.au>
2026-02-23 20:27:38 +1030

    Initial Commit

---
+++ TepariTests/TepariTests.swift
@@ -0,0 +1,17 @@
+//
+//  TepariTests.swift
+//  TepariTests
+//
+//  Created by Ashley Williams on 23/2/2026.
+//
+
+import Testing
+@testable import Tepari
+
+struct TepariTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
